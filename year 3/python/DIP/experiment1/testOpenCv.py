88df6b47a8fbe87ccab6c01a87f0dbfe5cce7182
samarmaharaj <samar7530933774samar@gmail.com>
2026-01-19 16:49:55 +0530

    updated on 19Jan2026

---
+++ year 3/python/DIP/experiment1/testOpenCv.py
@@ -0,0 +1,9 @@
+import cv2
+
+image = cv2.imread('colorImg.png')
+
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+cv2.imshow('Grayscale', gray_image)
+cv2.waitKey(0)  
+cv2.destroyAllWindows()
